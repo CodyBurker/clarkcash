a6f1fa96ff1c9b342d6ed83fc4457d95a6d03ad1
Cody <codyburker@gmail.com>
2023-09-26 18:39:01 -0400

    Change views to be class based

--- warriorwonders/pointstracking/urls.py
+++ warriorwonders/pointstracking/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views
 app_name = 'pointstracking'
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('students/<int:student_id>/', views.student, name='student'),
-    path('schools/<int:school_id>/', views.school, name='school'),
-    path('teachers/<int:teacher_id>/', views.teacher, name='teacher'),
+    path('', views.IndexView.as_view(), name='index'),
+    # path('students/<int:student_id>/', views.student, name='student'),
+    # path('schools/<int:school_id>/', views.school, name='school'),
+    path('teachers/<int:pk>/', views.TeacherView.as_view(), name='teacher'),
 ]
--- warriorwonders/pointstracking/views.py
+++ warriorwonders/pointstracking/views.py
@@ -2,39 +2,20 @@ from django.shortcuts import render
 from django.http import HttpResponse, Http404
 from .models import Student, Teacher, School
 from django.db.models import Prefetch
+from django.views.generic import DetailView, ListView
 
 
-def index(request):
-    schools = School.objects.all().prefetch_related( 
-                                                    Prefetch('teachers',queryset=Teacher.objects.order_by('grade__sort_order'))
-                                                    )
-    context = {'schools_list': schools}
-    return render(request, 'pointstracking/index.html', context)
-
-def student(request, student_id):
-    try:
-        student = Student.objects.get(pk=student_id)
-        # Create simple response with student string
-        return_string = str(student)
-        return HttpResponse(return_string)
+class IndexView(ListView):
+    model = School
+    template_name = 'pointstracking/index.html'
+    context_object_name = 'schools_list'
     
-    except Student.DoesNotExist:
-        raise Http404("Student does not exist")
+    def get_queryset(self):
+        return School.objects.all().prefetch_related(
+            Prefetch('teachers', queryset=Teacher.objects.order_by('grade__sort_order'))
+        )
 
-def school(request, school_id):
-    try:
-        school = School.objects.get(pk=school_id)
-        # Create simple response with school string
-        return_string = str(school)
-        return HttpResponse(return_string)
-    
-    except School.DoesNotExist:
-        raise Http404("School does not exist")
-
-def teacher(request, teacher_id):
-    try:
-        teacher = Teacher.objects.get(pk = teacher_id)
-        # Template teacher.html
-        return render(request, 'pointstracking/teacher.html', {'teacher': teacher}) 
-    except Teacher.DoesNotExist:
-        raise Http404("Teacher does not exist")
+class TeacherView(DetailView):
+    model = Teacher
+    template_name = 'pointstracking/teacher.html'
+    context_object_name = 'teacher'
